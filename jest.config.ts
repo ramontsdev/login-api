446e6ea7429e05249e44a4f113653594868ae61b
Ramon Nunes <ramontsdev@gmail.com>
2023-03-04 21:32:05 -0300

    chore: adicionado typescript e jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',],
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  // preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
