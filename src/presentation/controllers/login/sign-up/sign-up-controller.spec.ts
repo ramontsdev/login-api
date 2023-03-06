85b2b62118ce0d5131438ae0da1373c0d35e682a
Ramon Nunes <ramontsdev@gmail.com>
2023-03-06 09:40:53 -0300

    feat: chamar o EmailValidator com o valor correto

--- src/presentation/controllers/login/sign-up/sign-up-controller.spec.ts
+++ src/presentation/controllers/login/sign-up/sign-up-controller.spec.ts
@@ -1,10 +1,30 @@
 import { MissingParamError } from '../../../errors/missing-param-error';
 import { badRequest } from '../../../helpers/http-helpers';
+import { EmailValidator } from '../../../protocols/email-validator';
 import { SignUpController } from './sign-up-controller';
+
+function makeEmailValidator() {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
+}
+
+function makeSut() {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new SignUpController(emailValidatorStub);
+  return {
+    sut,
+    emailValidatorStub
+  };
+}
 
 describe('SignUp Controller', () => {
   test('Deveria retornar statusCode 400 se o name não for passado', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
 
     const httpResponse = await sut.handle({
       body: {
@@ -18,7 +38,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Deveria retornar statusCode 400 se o email não for passado', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
 
     const httpResponse = await sut.handle({
       body: {
@@ -32,7 +52,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Deveria retornar statusCode 400 se o password não for passado', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
 
     const httpResponse = await sut.handle({
       body: {
@@ -46,7 +66,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Deveria retornar statusCode 400 se o passwordConfirmation não for passado', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
 
     const httpResponse = await sut.handle({
       body: {
@@ -57,5 +77,23 @@ describe('SignUp Controller', () => {
     });
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordConfirmation')));
+  });
+
+
+
+  test('Deveria chamar o EmailValidator com o valor correto', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid');
+
+    await sut.handle({
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    });
+
+    expect(emailValidatorStub.isValid).toBeCalledWith('any_email@mail.com');
   });
 });
--- src/presentation/controllers/login/sign-up/sign-up-controller.ts
+++ src/presentation/controllers/login/sign-up/sign-up-controller.ts
@@ -1,9 +1,14 @@
 import { MissingParamError } from '../../../errors/missing-param-error';
 import { badRequest } from '../../../helpers/http-helpers';
 import { Controller } from '../../../protocols/controller';
+import { EmailValidator } from '../../../protocols/email-validator';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 
 export class SignUpController implements Controller {
+
+  constructor(
+    private readonly emailValidator: EmailValidator
+  ) { }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
@@ -16,5 +21,8 @@ export class SignUpController implements Controller {
       }
     }
 
+    const { email } = httpRequest.body;
+
+    this.emailValidator.isValid(email);
   }
 }
