27b21db0056463135a20f08ed82535774211beee
Ramon Nunes <ramontsdev@gmail.com>
2023-03-08 09:39:12 -0300

    feat: express route adapter

---
+++ src/main/factories/adapters/express-route-adapter.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+import { Controller } from '../../../presentation/protocols/controller';
+import { HttpRequest } from '../../../presentation/protocols/http';
+
+export function adaptRoute(controller: Controller) {
+
+  return async (request: Request, response: Response) => {
+    const httpRequest: HttpRequest = {
+      body: request.body
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+
+    if (httpResponse.statusCode >= 400) {
+      return response.status(httpResponse.statusCode).json({ error: httpResponse.body.message });
+    }
+
+    return response.status(httpResponse.statusCode).json(httpResponse.body);
+  };
+}
