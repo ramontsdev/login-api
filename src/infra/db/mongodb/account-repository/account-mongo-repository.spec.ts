dbbbe67bee228552beb7efde40a810d82d564eea
Ramon Nunes <ramontsdev@gmail.com>
2023-03-08 09:15:34 -0300

    feat: GetAccountByEmailRepository

--- src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -4,6 +4,8 @@ import { AccountMongoRepository } from './account-mongo-repository';
 
 let accountCollection: Collection;
 
+const sut = new AccountMongoRepository();
+
 describe('AccountMongo Repository', () => {
   beforeAll(async () => {
     await mongoHelper.connect(mongoUri);
@@ -20,7 +22,6 @@ describe('AccountMongo Repository', () => {
 
   describe('CreateAccountRepository', () => {
     test('Deveria retornar uma conta em caso de sucesso', async () => {
-      const sut = new AccountMongoRepository();
       const account = await sut.create({
         name: 'any_name',
         email: 'any_email@mail.com',
@@ -32,6 +33,26 @@ describe('AccountMongo Repository', () => {
       expect(account.name).toBe('any_name');
       expect(account.email).toBe('any_email@mail.com');
       expect(account.password).toBe('any_password');
+    });
+  });
+
+  describe('GetAccountByEmailRepository', () => {
+    test('Deveria retornar uma conta em caso de sucesso', async () => {
+      const sut = new AccountMongoRepository();
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      });
+
+      const account = await sut.getByEmail('any_email@mail.com');
+
+      expect(account).toBeTruthy();
+      expect(account?.id).toBeTruthy();
+      expect(account?.name).toBe('any_name');
+      expect(account?.email).toBe('any_email@mail.com');
+      expect(account?.password).toBe('any_password');
     });
   });
 });
--- src/infra/db/mongodb/account-repository/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.ts
@@ -1,12 +1,16 @@
+
 import { CreateAccountRepository } from '../../../../data/protocols/db/create-account-repository';
+import { GetAccountByEmailRepository } from '../../../../data/protocols/db/get-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account-model';
 import { CreateAccountModel } from '../../../../domain/models/create-account-model';
 import { mongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements CreateAccountRepository {
+export class AccountMongoRepository implements
+  CreateAccountRepository,
+  GetAccountByEmailRepository {
 
   async create(accountData: CreateAccountModel): Promise<AccountModel> {
-    const accountCollection = await mongoHelper.getCollection<CreateAccountModel>('accounts');
+    const accountCollection = await mongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
     const account = await accountCollection.findOne({ _id: result.insertedId });
 
@@ -14,9 +18,17 @@ export class AccountMongoRepository implements CreateAccountRepository {
       throw new Error('Documento não encontrado');
     }
 
-    const { _id, ...res } = account;
-    const newAccount = Object.assign({}, res, { id: _id.toString() });
+    return mongoHelper.mapDocument<AccountModel>(account);
+  }
 
-    return newAccount;
+  async getByEmail(email: string): Promise<AccountModel | null> {
+    const accountCollection = await mongoHelper.getCollection<CreateAccountModel>('accounts');
+    const result = await accountCollection.findOne({ email });
+
+    if (result) {
+      return mongoHelper.mapDocument<AccountModel>(result);
+    }
+
+    return null;
   }
 }
