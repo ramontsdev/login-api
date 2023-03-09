ba8db9dd5e31cf0b39208f0d19802d917ab87f5e
Ramon Nunes <ramontsdev@gmail.com>
2023-03-08 21:22:35 -0300

    feat: passando um objeto com o id para o sign

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -5,7 +5,7 @@ import { Encrypter } from '../../data/protocols/cryptography/encrypter';
 export class JwtAdapter implements Encrypter {
   constructor(private readonly secret: string) { }
 
-  async encrypt(value: string): Promise<string> {
-    return jwt.sign(value, this.secret);
+  async encrypt(id: string): Promise<string> {
+    return jwt.sign({ id }, this.secret);
   }
 }
